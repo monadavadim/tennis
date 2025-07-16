5fa29d5a3d3814b60061243b2ad35840a029e9b0
Вадим Монада <vammaa2017@gmail.com>
2025-07-16 15:10:34 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
+
 const burger = document.getElementById('burger');
 const headerNav = document.getElementById('header__nav');
 
